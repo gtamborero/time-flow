3298143fd1f01e4d5e3d4b35df1c4523fdd62ce3
guillermo <gtamborero@iproject.cat>
2020-03-31 01:24:24 +0200

    tailwind config initial colors for timeflow

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {},
+    colors: {
+      primary: '#2a4365',
+      'primary-light': '#3a5375',
+      secondary: '#007ace',
+      white: '#fff',
+      black: '#000',
+      'gray-100': '#F7FAFC',
+      'gray-200': '#EDF2F7',
+      'gray-300': '#E2E8F0',
+      'gray-400': '#CBD5E0',
+    }
   },
   variants: {},
   plugins: [
-    require('@tailwindcss/custom-forms'),
-    require('autoprefixer')
+    require('@tailwindcss/custom-forms')
   ]
 }
