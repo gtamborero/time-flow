b8fa212af8eba5c9254f84f1bc4eec5a454c183b
guillermo <gtamborero@iproject.cat>
2020-04-01 00:49:47 +0200

    Initial View adjusts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,32 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="flex">
-        <div class="flex-1">
 
-            @if (session('status'))
-                <div class="text-sm border border-t-8 rounded text-green-700 border-green-600 bg-green-100 px-3 py-4 mb-4" role="alert">
-                    {{ session('status') }}
-                </div>
-            @endif
+  @if (session('status'))
+      <div class="col-span-2 p-6 bg-gray-400 text-center" role="alert">
+          {{ session('status') }}
+      </div>
+  @endif
 
-        </div>
-    </div>
-
-    <div class="grid sm:grid-cols-2">
-        <div class=" p-6 break-words bg-white shadow-md">
-          @auth
-            <img class="rounded-full" src="{{ gravatar(Auth::user()->email) }}">
-          @endauth
-
-          @guest
-            <p> Nos conocemos?</p>
-          @endguest
-        </div>
-        <div class="p-6 break-words bg-white shadow-md">
-        Col2
-        </div>
-    </div>
-
+  <x-user-heading/>
 
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,24 +12,30 @@
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
-<body class="bg-gray-100 h-screen antialiased leading-none">
+<body class="bg-gray-100 h-screen antialiased">
     <div id="app">
         <nav class="bg-primary shadow mb-0 sm:mb-6" style="padding:32px 0;">
             <div class="container mx-auto px-6">
                 <div class="flex items-center justify-center">
                     <div class="mr-6">
                         <a href="{{ url('/') }}" class="text-xl font-semibold text-gray-100 no-underline">
-                            {{ config('app.name', 'Laravel') }}
+                          <img class="rounded-full mx-auto inline h-12 w-12 mr-2" src="
+                          @auth
+                            {{ gravatar(Auth::user()->email) }}
+                          @else
+                            {{-- imagen logo si no estas registrado --}}
+                          @endauth
+
+                          "> {{ config('app.name', 'Laravel') }}
                         </a>
                     </div>
                     <div class="flex-1 text-right">
                         @guest
                             <a class="text-lg no-underline hover:underline text-gray-300 text-sm p-3" href="{{ route('login') }}">{{ __('Login') }}</a>
                         @else
-                            <span class="text-gray-300 text-sm pr-4">{{ Auth::user()->name }}</span>
 
                             <a href="{{ route('logout') }}"
-                               class="no-underline hover:underline text-gray-300 text-sm p-3"
+                               class="text-lg no-underline hover:underline text-gray-300 text-sm p-3"
                                onclick="event.preventDefault();
                                     document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
