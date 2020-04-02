e64cc9dcc2381f49763aa37f99d8357e073fab3d
guillermo <gtamborero@iproject.cat>
2020-04-03 01:51:52 +0200

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([UsersTableSeeder::class]);
-        $this->call(ExchangesSeeder::class);
+        $this->call(
+          [
+            UsersTableSeeder::class,
+            ExchangesSeeder::class
+          ]
+        );
     }
 }
