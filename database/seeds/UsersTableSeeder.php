6d3996b7ada32ef8da9e4cb75ad25de4631bb7f8
guillermo <gtamborero@iproject.cat>
2020-04-13 22:25:09 +0200

    Update UsersTableSeeder.php

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,10 +12,37 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
       DB::table('users')->insert([
+        [
         'id' => 1,
         'name' => 'guille',
         'email' => 'gtamborero@iproject.cat',
         'password' => Hash::make('iProject77')
+        ],
+        [
+        'id' => 2,
+        'name' => 'xavi',
+        'email' => 'xavier@iproject.cat',
+        'password' => Hash::make('iProject77')
+        ],
+        [
+        'id' => 3,
+        'name' => 'pedro',
+        'email' => 'pedro@iproject.cat',
+        'password' => Hash::make('iProject77')
+        ],
+        [
+        'id' => 4,
+        'name' => 'jorge',
+        'email' => 'jorge@iproject.cat',
+        'password' => Hash::make('iProject77')
+        ],
+        [
+        'id' => 5,
+        'name' => 'paco',
+        'email' => 'paco@iproject.cat',
+        'password' => Hash::make('iProject77')
+        ],
+
       ]);
     }
 }
