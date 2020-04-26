92bf9a4215330aba17915026a1b9d0ab564cda53
guillermo <gtamborero@iproject.cat>
2020-04-27 01:53:17 +0200

    Update StarRating.vue
    WITHOUT THE VUEJS Props Mutating Warning -> https://codepen.io/gtamborero/pen/vYNmjPV

--- resources/js/components/StarRating.vue
+++ resources/js/components/StarRating.vue
@@ -1,14 +1,15 @@
 <template>
   <!-- component from: https://codepen.io/olimorris/pen/yOYBjd -->
   <div class="star-rating">
-          <label class="star-rating__star" v-for="rating in ratings"
-          :class="{'is-selected': ((temp_value >= rating) && temp_value != null),
-          'is-disabled': disabled}"
-          v-on:click="set(rating)" v-on:mouseover="star_over(rating)"
-          v-on:mouseout="star_out">
+      <label class="star-rating__star" v-for="rating in ratings"
+      :class="{'is-selected': ((tempValue >= rating) && tempValue != null),
+      'is-disabled': disabled}"
+      v-on:click="set(rating)" v-on:mouseover="star_over(rating)"
+      v-on:mouseout="star_out">
           <input class="star-rating star-rating__checkbox"
           type="radio" :name="name"
-          :value="finalValue" :disabled="disabled">★</label></div>
+          :value="finalValue" :disabled="disabled">★</label>
+  </div>
 </template>
 
 <script>
@@ -22,11 +23,10 @@
       },
       mounted() {
           //console.log('Component mounted.')
-
       },
       data: function() {
          return {
-           temp_value: this.value,
+           tempValue: this.value,
            finalValue: this.value,
            ratings: [1, 2, 3, 4, 5]
          };
@@ -36,21 +36,17 @@
           * Behaviour of the stars on mouseover.
           */
          star_over: function(index) {
-
            if (!this.disabled) {
-             //this.temp_value = this.value;
-             return this.temp_value = index;
+             return this.tempValue = index;
            }
-
          },
 
          /*
           * Behaviour of the stars on mouseout.
           */
          star_out: function() {
-
            if (!this.disabled) {
-             return this.temp_value = this.finalValue;
+             return this.tempValue = this.finalValue;
            }
          },
 
@@ -58,13 +54,8 @@
           * Set the rating.
           */
          set: function(value) {
-
-
            if (!this.disabled) {
-
              this.finalValue = value;
-             //return this.temp_value = index;
-             //this.temp_value = value;
            }
          }
        }
