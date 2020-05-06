71b55e39202c4e6002217d5507405dc643ef3a7d
guillermo <gtamborero@iproject.cat>
2020-05-06 23:03:21 +0200

    Export store from app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,20 +1,10 @@
 /**
  * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
+ * includes Vue and other libraries.
  */
 
 require('./bootstrap');
-
 window.Vue = require('vue');
-
-
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
 
 import ExchangeBlock from './components/ExchangeBlock.vue';
 Vue.component('exchange-block', ExchangeBlock);
@@ -31,46 +21,26 @@ Vue.component('star-rating', StarRating);
 import CommentExchange from './components/commentExchange.vue';
 Vue.component('comment-exchange', CommentExchange);
 
+import Vuex from 'vuex';
+Vue.use(Vuex);
+
+// Get laravel Data from blade <script>
+import StoreData from './store.vue';
+const store = new Vuex.Store(StoreData);
+
 // Use internationalization -> martinlindhe/laravel-vue-i18n-generator
 import VueInternationalization from 'vue-i18n';
 import Locale from './vue-i18n-locales.generated';
 
-import Vuex from 'vuex';
-
+// VueInternationalization config
 Vue.use(VueInternationalization);
-Vue.use(Vuex);
-
- //determine your current app locale
- const lang = document.documentElement.lang.substr(0, 2);
-
- const i18n = new VueInternationalization({
+const lang = document.documentElement.lang.substr(0, 2);
+const i18n = new VueInternationalization({
      locale: lang,
      messages: Locale
  });
 
-
-const store = new Vuex.Store({
- state: {
-   exchange:window.iproexchange
- },
- mutations: {
-   setRating (state, payload) {
-     state.exchange[payload.id].rating = payload.rating;
-   },
-   setComment (state, payload) {
-     state.exchange[payload.id].comment = payload.comment;
-   },
- },
- getters: {
-    getRating: (state) => (id) => {
-      return state.exchange[id].rating;
-    },
-    getComment: (state) => (id) => {
-      return state.exchange[id].comment;
-    },
-  }
-});
-
+// START VUE!
  const app = new Vue({
      el: '#app',
      store,
---
+++ resources/js/store.vue
@@ -0,0 +1,22 @@
+<script>export default {
+ state: {
+   exchange:window.iproexchange
+ },
+ mutations: {
+   setRating (state, payload) {
+     state.exchange[payload.id].rating = payload.rating;
+   },
+   setComment (state, payload) {
+     state.exchange[payload.id].comment = payload.comment;
+   },
+ },
+ getters: {
+    getRating: (state) => (id) => {
+      return state.exchange[id].rating;
+    },
+    getComment: (state) => (id) => {
+      return state.exchange[id].comment;
+    },
+  }
+};
+</script>
