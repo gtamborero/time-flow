c7d48b5c31d5259414f1ca93494305ea98fdb493
guillermo <gtamborero@iproject.cat>
2020-05-10 23:39:04 +0200

    Adjusting userRating

--- resources/js/components/UserRating.vue
+++ resources/js/components/UserRating.vue
@@ -3,8 +3,8 @@
   <!-- LINE COMMENTS + STARS -->
   <div  class="grid grid-cols-1 bg-white break-words">
     <div class="pb-3 pt-0 px-5 text-center star-container">
-      <star-rating :id="id" :temp-value="rating"></star-rating>
-      {{ userName }} {{ $t('said') }}: {{comment}}
+      <star-rating :id="id" :temp-value="getRating(id)" disabled></star-rating>
+      {{ userName }} {{ $t('said') }}: {{getComment(id)}}
     </div>
   </div>
 </div>
@@ -17,19 +17,6 @@
         'id',
         'user-name'
       ],
-      data: function(){
-        return {
-          comment: '',
-          rating: 0
-        }
-      },
-      mounted() {
-        this.comment = this.getComment(this.id);
-        this.rating = this.getRating(this.id);
-          //console.log('Component mounted.')
-      },
-      methods: {
-      },
       computed: mapGetters(['getRating','getComment'])
     }
 </script>
