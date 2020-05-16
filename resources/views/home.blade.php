6bfd03fd5817c394610eb9ab79b2869f6028efde
guillermo <gtamborero@iproject.cat>
2020-05-17 01:36:39 +0200

    inital data set Comment+Rate : check if exists

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,10 +24,32 @@
 <script>
     window.iproexchange = [];
       @foreach ($exchanges as $exchange)
+
+        <?php
+        // Initial data set and check if exists
+        if ($exchange->getRating){
+          $rating = $exchange->getRating->rating;
+        }else{
+          $rating = 0;
+        }
+
+        if ($exchange->getRating){
+          $comment = $exchange->getRating->comment;
+        }else{
+          $comment = "";
+        }
+
+        if ($exchange->status){
+          $status = $exchange->status;
+        }else{
+          $status = 0;
+        }
+        ?>
+
         window.iproexchange[{{$exchange->id}}] = {
-            rating: @if ($exchange->getRating->rating){{ $exchange->getRating->rating }}@else 0 @endif,
-            comment: "@if ($exchange->getRating){{ $exchange->getRating->comment }}@endif",
-            status: "@if ($exchange->status){{ $exchange->status }}@endif"
+            rating: {{ $rating }},
+            comment: "{{ $comment }}",
+            status: "{{ $status }}"
         }
       @endforeach
 </script>
