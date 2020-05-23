4aa78b99292894fe809609c16059e49b3620ef48
guillermo <gtamborero@iproject.cat>
2020-05-23 23:26:32 +0200

    create Constants file

---
+++ config/constants.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+// GLOBAL CONSTANTS for TIMEBANK
+// use example: if (Constant::STATUS_ACCEPTED) ...
+
+class Constant {
+   const STATUS_PENDING = 0;
+   const STATUS_ACCEPTED = 1;
+   const STATUS_REJECTED = -1;
+}
