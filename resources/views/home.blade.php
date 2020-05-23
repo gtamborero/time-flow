8f30015580ab4b690fc1a141973612e9bc37bfa1
guillermo <gtamborero@iproject.cat>
2020-05-23 22:50:19 +0200

    export script injection to partial

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,38 +21,7 @@
     ?>
 
     {{-- Feed exchange data --}}
-    <script>
-      window.iproexchange = [];
-      @foreach ($exchanges as $exchange)
-
-        <?php
-        // Initial data set and check if exists
-        if ($exchange->getRating){
-          $rating = $exchange->getRating->rating;
-        }else{
-          $rating = 0;
-        }
-
-        if ($exchange->getRating){
-          $comment = $exchange->getRating->comment;
-        }else{
-          $comment = "";
-        }
-
-        if ($exchange->status){
-          $status = $exchange->status;
-        }else{
-          $status = 0;
-        }
-        ?>
-
-        window.iproexchange[{{$exchange->id}}] = {
-            rating: {{ $rating }},
-            comment: "{{ $comment }}",
-            status: "{{ $status }}"
-        }
-      @endforeach
-    </script>
+    @include ('partials.script-injection-vue')
 
       @foreach ($exchanges as $exchange)
         <div class="mb-6 shadow-md">
