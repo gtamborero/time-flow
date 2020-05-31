749d230465902e48bdbb7995a0beaf0964c56742
guillermo <gtamborero@iproject.cat>
2020-06-01 00:27:16 +0200

    sweet alert with input fields for request / send

--- resources/js/components/request-send-button.vue
+++ resources/js/components/request-send-button.vue
@@ -1,15 +1,16 @@
 <template>
-  <div v-if="renderComponent" class="fixed bottom-0 color1 left-0 right-0 grid grid-cols-2">
-    <a href="http://time-flow.test/search">
-      <button class="fixed-button capitalize">
+  <div v-if="renderComponent"
+    class="fixed bottom-0 p-1 bg-white color1 left-0 right-0 grid grid-cols-2"
+    style="border-top:1px solid #ccc;"
+    >
+      <button v-on:click="createNew()"
+        class="fixed-button capitalize">
         REQUEST TIME TO<br> {{profileUserName}}
       </button>
-    </a>
-    <a href="http://time-flow.test/search">
-      <button class="fixed-button capitalize">
+      <button v-on:click="createNew()"
+        class="fixed-button capitalize">
         SEND TIME TO<br>  {{profileUserName}}
       </button>
-    </a>
   </div>
 </template>
 
@@ -30,6 +31,23 @@
          if (this.profileUserId != this.actualUserId) this.renderComponent = 1;
        },
       methods: {
+        createNew: function (){
+
+          const div = document.createElement("div");
+          div.innerHTML ="<input type=range id='111' value='one'></input></br>second<input id='222' value='two'></input></br>third<input id='333' value='three'></input>";
+          //console.log(div);
+
+          this.$swal({
+            title: this.$t('Request time to XXX'),
+            //icon: "success",
+            text: this.$t('You\'re logged in'),
+            content: div,
+            buttons: [true, "Create"]
+          }).then((value) => {
+            console.log(value);
+            //swal(`You typed: ${value}`);
+          });
+        }
       }
      }
 </script>
