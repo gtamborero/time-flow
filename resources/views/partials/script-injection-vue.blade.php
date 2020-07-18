c92423663cb6bfd7aae56eb8b7a7503611b19bd6
guillermo <gtamborero@iproject.cat>
2020-07-18 02:03:32 +0200

    Update script-injection-vue.blade.php

--- resources/views/partials/script-injection-vue.blade.php
+++ resources/views/partials/script-injection-vue.blade.php
@@ -1,32 +1,30 @@
 <script>
-  window.iproexchange = [];
-  @foreach ($exchanges as $exchange)
+window.iproexchange = [];
+@foreach ($exchanges as $exchange)
+  <?php
+  // Initial data set and check if exists
+  if ($exchange->getRating){
+    $rating = $exchange->getRating->rating;
+  }else{
+    $rating = 0;
+  }
 
-    <?php
-    // Initial data set and check if exists
-    if ($exchange->getRating){
-      $rating = $exchange->getRating->rating;
-    }else{
-      $rating = 0;
-    }
+  if ($exchange->getRating){
+    $comment = $exchange->getRating->comment;
+  }else{
+    $comment = "";
+  }
 
-    if ($exchange->getRating){
-      $comment = $exchange->getRating->comment;
-    }else{
-      $comment = "";
-    }
-
-    if ($exchange->status){
-      $status = $exchange->status;
-    }else{
-      $status = 0;
-    }
-    ?>
-
-    window.iproexchange[{{$exchange->id}}] = {
-        rating: {{ $rating }},
-        comment: "{{ $comment }}",
-        status: "{{ $status }}"
-    }
+  if ($exchange->status){
+    $status = $exchange->status;
+  }else{
+    $status = 0;
+  }
+  ?>
+window.iproexchange[{{$exchange->id}}] = {
+    rating: {{ $rating }},
+    comment: "{{ $comment }}",
+    status: "{{ $status }}"
+}
   @endforeach
 </script>
