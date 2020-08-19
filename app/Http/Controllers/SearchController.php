0ccfcaef8777fd329da803b20bb3aec1187c6b26
guillermo <gtamborero@iproject.cat>
2020-08-20 00:53:56 +0200

    Algolia testing -> OK!

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class SearchController extends Controller
 {
     public function index(){
+      $data = User::search('informático')->get();
+      dd($data);
       return view('search');
     }
 }
