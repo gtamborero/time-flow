bd2bda5c7093ab1370f471c966052a00d4f3faa0
guillermo <gtamborero@iproject.cat>
2020-09-25 10:43:47 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-		if (App::environment('production')){
+		if (\App::environment('production')){
 		  $this->app->bind('path.public', function() {
 				return base_path().'/../html';
 			});
