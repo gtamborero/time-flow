56ebe260909155c648c9f75b3610cac794454743
guillermo <gtamborero@iproject.cat>
2020-10-05 00:59:31 +0200

    Update services.php

--- config/services.php
+++ config/services.php
@@ -33,7 +33,7 @@ return [
     'facebook' => [
       'client_id' => env('FACEBOOK_CLIENT_ID'),
       'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-      'redirect' => 'http://your-callback-url',
+      'redirect' => 'https://time-flow.app',
     ],
 
 ];
