36204197ae496938df0689d4ae6afae08c5b7ee0
guillermo <gtamborero@iproject.cat>
2020-10-05 00:29:57 +0200

    block user case revision "you havent done exchanges yet"

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -22,6 +22,7 @@
     {{-- Feed exchange data --}}
     @include ('partials.script-injection-vue')
 
+      {{-- Show exchanges or --}}
       @forelse ($exchanges as $exchange)
         <div class="mb-5 shadow-md">
             <exchange-block
@@ -38,21 +39,33 @@
               >
             </exchange-block>
         </div>
+
+      {{-- Show Call to action to actual user or --}}
       @empty
-        <div class="p-4 my-5 break-words text-2xl text-center bg-gray-300">
-          @lang('You don\'t have any exchanges yet').<br>
-          @lang('Start searching users or skills').<br>
-          @lang('Connect with users and start paying time')!<br><br>
-          <button class="tf-button tf-button-primary text-lg uppercase">
-            <a target=_BLANK href="https://www.youtube.com/watch?v=1KY7b-C-HHA">
-              @lang('Play promotional video')
-            </a>
-          </button>
-        </div>
+        <?php if (isVerifiedUser() && ($userName == Auth::user()->name)){ ?>
+          <div class="p-4 my-5 break-words text-2xl text-center bg-gray-300">
+            @lang('You don\'t have any exchanges yet').<br>
+            @lang('Start searching users or skills').<br>
+            @lang('Connect with users and start paying time')!<br><br>
+            <button class="tf-button tf-button-primary text-lg uppercase">
+              <a target=_BLANK href="https://www.youtube.com/watch?v=1KY7b-C-HHA">
+                @lang('Play promotional video')
+              </a>
+            </button>
+          </div>
+
+        {{-- Show Call to action with the viewed user --}}
+        <?php }else{ ?>
+          <div class="p-4 my-5 break-words text-2xl text-center bg-gray-300">
+            <span class="capitalize">{{ $userName }}</span> @lang('hasn\'t done exchanges yet').<br>
+            @lang('Be the first one')!
+          </div>
+        <?php } ?>
       @endforelse
 
       @include ('partials.search-block')
 
+      {{-- Show PAYMENT fixed down button --}}
       <?php if (isVerifiedUser() && ($userName != Auth::user()->name)){ ?>
       <request-send-button
         :profile-user-id="{{$userId}}"
