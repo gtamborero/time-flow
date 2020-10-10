1053a9bf896eee50b8c45e7fce24df83466bafb3
guillermo <gtamborero@iproject.cat>
2020-10-11 00:30:15 +0200

    for production: app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+		if (\App::environment('production')){
+		  $this->app->bind('path.public', function() {
+				return base_path().'/../html';
+			});
+		}
     }
 
     /**
