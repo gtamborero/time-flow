d34a7dc152407e2774ab44a599ddc512364750af
guillermo <gtamborero@iproject.cat>
2020-10-26 12:11:06 +0100

    Update HumanizeMinutes.php

--- app/Helpers/HumanizeMinutes.php
+++ app/Helpers/HumanizeMinutes.php
@@ -1,5 +1,11 @@
 <?php
 function humanizeMinutes($minutes){
-    if (date('H', mktime(0, $minutes)) == 0) return date('i', mktime(0, $minutes)) . __('min');
-    return date('H', mktime(0, $minutes)) . ':' . date('i', mktime(0, $minutes)) . __('h');
+  $sign = sign($minutes);
+  $minutes = abs($minutes);
+    if (date('H', mktime(0, $minutes)) == 0) return $sign . date('i', mktime(0, $minutes)) . __('min');
+    return $sign . date('H', mktime(0, $minutes)) . ':' . date('i', mktime(0, $minutes)) . __('h');
+}
+
+function sign($number) {
+  if ($number < 0) return "-";
 }
