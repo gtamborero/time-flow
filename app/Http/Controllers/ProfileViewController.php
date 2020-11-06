1244433fb57aaf6979bc5942f205fd10f05cb2f4
guillermo <gtamborero@iproject.cat>
2020-11-06 11:58:54 +0100

    Update ProfileViewController.php

--- app/Http/Controllers/ProfileViewController.php
+++ app/Http/Controllers/ProfileViewController.php
@@ -41,4 +41,33 @@ class ProfileViewController extends Controller
         ->with('userName',$userName)
         ->with('exchanges',$exchanges);
   }
+
+  public function homeuser($userName)
+  {
+      $userData = \App\User::where('name', $userName)->first();
+
+      // Check if user has verified mail:
+      if ((Auth::user()) && ($userName == Auth::user()->name)){
+        if (!isVerifiedUser()) return redirect ('email/verify');
+      }
+
+      $userId = $userData->id;
+
+      $exchanges = \App\Exchanges
+          ::where('id_buyer', $userId)
+          ->orWhere('id_seller', $userId)
+          ->with('getSellerUser')
+          ->with('getBuyerUser')
+          ->with('getCreatorUser')
+          ->with('getRating')
+          ->orderBy('id', 'desc')
+          ->get();
+
+      return view('profile')
+        ->with('userId',$userId)
+        ->with('userData',$userData)
+        ->with('userName',$userName)
+        ->with('exchanges',$exchanges)
+        ->with('homeuser',1);
+  }
 }
