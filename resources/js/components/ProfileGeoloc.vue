dba93f941b5179b406dd60361871949b5c8ba11f
guillermo <gtamborero@iproject.cat>
2020-11-06 11:11:18 +0100

    improve vue component geolocation

--- resources/js/components/ProfileGeoloc.vue
+++ resources/js/components/ProfileGeoloc.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-<button @click.prevent="getLocation" class="tf-button tf-button-primary mt-4 mx-auto uppercase">GeoLocate Me!</button>
+<button @click.prevent="getLocation" class="tf-button tf-button-primary mx-auto uppercase">GeoLocate Me!</button>
 </div>
 </template>
 
@@ -54,21 +54,6 @@
           }
       },
 
-      saveUserLocation: function(locationData){
-        axios.post('/save-user-location', {
-            country: locationData.adminArea1,
-            city: locationData.adminArea2 + " " + locationData.adminArea3,
-            town: locationData.adminArea4 + " " + locationData.adminArea5 + " " + locationData.adminArea6,
-            postalcode: locationData.postalCode
-        }).then(response => {
-            // Respuesta del servidor
-            console.log(response);
-        }).catch(e => {
-            console.log(e);
-        });
-
-      },
-
       showPosition: function (position) {
         //console.log("Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude);
         var app = this;
@@ -77,34 +62,35 @@
           url: 'https://www.mapquestapi.com/geocoding/v1/reverse?key=' + this.mapKey + '&location=' + position.coords.latitude + ',' + position.coords.longitude + '&includeRoadMetadata=true&includeNearestIntersection=true',
           }).then(function(response) {
 
-            console.log(response.data.results[0].locations[0]);
-            var location = document.getElementById("location");
             var locationData = response.data.results[0].locations[0];
+            console.log(locationData);
+
             var locationField = document.getElementById('locationField');
 
+            // Set country + city
             if (locationData.adminArea1){
-              locationField.innerHTML = locationData.adminArea1 + " - ";
+              document.getElementById('country').value=locationData.adminArea1;
             }
             if (locationData.adminArea2){
-              locationField.innerHTML += locationData.adminArea2 + " - ";
-            }
-            if (locationData.adminArea3){
-              locationField.innerHTML += locationData.adminArea3 + " - ";
-            }
-            if (locationData.adminArea4){
-              locationField.innerHTML = locationData.adminArea4 + " - ";
-            }
-            if (locationData.adminArea5){
-              locationField.innerHTML += locationData.adminArea5 + " - ";
-            }
-            if (locationData.adminArea6){
-              locationField.innerHTML += locationData.adminArea6 + " - ";
-            }
-            if (locationData.postalCode){
-              locationField.innerHTML += locationData.postalCode;
+              document.getElementById('city').value=locationData.adminArea2;
+            }else
+            if(locationData.adminArea3){
+              document.getElementById('city').value=locationData.adminArea3;
             }
 
-            app.saveUserLocation(locationData);
+            // Show to user editable data:
+            if (locationData.adminArea4){
+              locationField.value = locationData.adminArea4 + " - ";
+            }
+            if (locationData.adminArea5){
+              locationField.value += locationData.adminArea5 + " - ";
+            }
+            if (locationData.adminArea6){
+              locationField.value += locationData.adminArea6 + " - ";
+            }
+            if (locationData.postalCode){
+              locationField.value += locationData.postalCode;
+            }
 
           }).catch(error => {
             console.log(error)
