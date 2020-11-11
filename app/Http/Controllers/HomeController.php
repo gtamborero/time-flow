5e9ebe459f91fc5b15fdfd161559cf198a7f05aa
guillermo <gtamborero@iproject.cat>
2020-11-11 23:53:31 +0100

    delete deprecated method

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -39,7 +39,7 @@ class HomeController extends Controller
 
       // IF User is Logged in
       if (isset(Auth::user()->name)){
-        return redirect()->route('homeuserView',Auth::user()->name);
+        return redirect()->route('profileView',Auth::user()->name);
       }
 
       // IF Anonymouse user
