c59c5e8b88746239bdd8211f5f6e32c063bb4c17
guillermo <gtamborero@iproject.cat>
2020-11-29 00:16:47 +0100

    confing laravelpwa

--- config/laravelpwa.php
+++ config/laravelpwa.php
@@ -12,32 +12,8 @@ return [
         'orientation'=> 'any',
         'status_bar'=> 'black',
         'icons' => [
-            '72x72' => [
-                'path' => '/images/icons/icon-72x72.png',
-                'purpose' => 'any'
-            ],
-            '96x96' => [
-                'path' => '/images/icons/icon-96x96.png',
-                'purpose' => 'any'
-            ],
-            '128x128' => [
-                'path' => '/images/icons/icon-128x128.png',
-                'purpose' => 'any'
-            ],
-            '144x144' => [
-                'path' => '/images/icons/icon-144x144.png',
-                'purpose' => 'any'
-            ],
-            '152x152' => [
-                'path' => '/images/icons/icon-152x152.png',
-                'purpose' => 'any'
-            ],
             '192x192' => [
                 'path' => '/images/icons/icon-192x192.png',
-                'purpose' => 'any'
-            ],
-            '384x384' => [
-                'path' => '/images/icons/icon-384x384.png',
                 'purpose' => 'any'
             ],
             '512x512' => [
