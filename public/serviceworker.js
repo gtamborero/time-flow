f962fe6bcf9761d3f0b733793adf126db311aa0b
guillermo <gtamborero@iproject.cat>
2020-11-29 01:32:23 +0100

    remove unused icons

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,14 +1,10 @@
+var date = new Date();
+var staticCacheName = "pwa-v";
 var filesToCache = [
     '/offline',
     '/css/app.css',
     '/js/app.js',
-    '/images/icons/icon-72x72.png',
-    '/images/icons/icon-96x96.png',
-    '/images/icons/icon-128x128.png',
-    '/images/icons/icon-144x144.png',
-    '/images/icons/icon-152x152.png',
     '/images/icons/icon-192x192.png',
-    '/images/icons/icon-384x384.png',
     '/images/icons/icon-512x512.png',
 ];
 
