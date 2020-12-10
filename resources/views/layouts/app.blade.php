75050b9ab9a190a75a5a17f20229029ad63e3462
guillermo <gtamborero@iproject.cat>
2020-12-10 22:07:09 +0100

    partial for menu buttons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,65 +58,7 @@
         <div class="container mx-auto px-0 sm:px-6 pb-6">
           @yield('content')
         </div>
-
-        <div class="fixed bottom-0 left-0 right-0" style="background:#000;">
-          <div class="flex container mx-auto p-2 text-white">
-
-            <a class="flex-1 text-center" href="@isset (Auth::user()->name)
-                {{ route('userExchanges', Auth::user()->name) }}
-              @else
-                {{ url('last-exchanges') }}
-              @endif">
-              <button class="head-button" style="padding-top:13px;">
-                <span class="icon-exchange text-white" style="font-size: 1.5rem; margin-bottom:1px;"></span>
-                <span class="hidden md:block">
-                  @lang('Exchanges')
-                </span>
-              </button>
-            </a>
-
-            <a class="flex-1 text-center" href="{{ route('showAllUsers') }}">
-              <button class="head-button">
-                <span class="icon-profile text-white text-3xl"></span>
-                <span class="hidden md:block capitalize">
-                  @lang('Users')
-                </span>
-              </button>
-            </a>
-
-            <a class="flex-1 text-center" href="{{ route('search') }}">
-              <button class="head-button" style="padding-top:6px;">
-                <span class="icon-search text-white" style="font-size: 2rem;"></span>
-                <span class="hidden md:block">
-                  @lang('Search')
-                </span>
-              </button>
-              </a>
-
-            @guest
-              <a class="flex-1 text-center" href="{{ route('login') }}">
-                <button class="head-button">
-                  <span class="icon-person text-white text-3xl"></span>
-                    <span class="hidden md:block capitalize">
-                      {{ __('Login') }}
-                    </span>
-                </button>
-              </a>
-            @else
-
-            <a class="flex-1 text-center" href="{{ route('profileView', Auth::user()->name) }}">
-              <button class="head-button">
-                <span class="icon-person text-white text-3xl"></span>
-                <span class="hidden md:block capitalize">
-                  {{Auth::user()->name}}
-                </span>
-              </button>
-            </a>
-
-            @endguest
-
-          </div>
-        </div>
+        @include('partials.menu-buttons')
     </div>
 
     <script src="{{ mix('js/app.js') }}"></script>
---
+++ resources/views/partials/menu-buttons.blade.php
@@ -0,0 +1,58 @@
+<div class="fixed bottom-0 left-0 right-0" style="background:#000;">
+  <div class="flex container mx-auto p-2 text-white">
+
+    <a class="flex-1 text-center" href="@isset (Auth::user()->name)
+        {{ route('userExchanges', Auth::user()->name) }}
+      @else
+        {{ url('last-exchanges') }}
+      @endif">
+      <button class="head-button" style="padding-top:13px;">
+        <span class="icon-exchange text-white" style="font-size: 1.5rem; margin-bottom:1px;"></span>
+        <span class="hidden md:block">
+          @lang('Exchanges')
+        </span>
+      </button>
+    </a>
+
+    <a class="flex-1 text-center" href="{{ route('showAllUsers') }}">
+      <button class="head-button">
+        <span class="icon-profile text-white text-3xl"></span>
+        <span class="hidden md:block capitalize">
+          @lang('Users')
+        </span>
+      </button>
+    </a>
+
+    <a class="flex-1 text-center" href="{{ route('search') }}">
+      <button class="head-button" style="padding-top:6px;">
+        <span class="icon-search text-white" style="font-size: 2rem;"></span>
+        <span class="hidden md:block">
+          @lang('Search')
+        </span>
+      </button>
+      </a>
+
+    @guest
+      <a class="flex-1 text-center" href="{{ route('login') }}">
+        <button class="head-button">
+          <span class="icon-person text-white text-3xl"></span>
+            <span class="hidden md:block capitalize">
+              {{ __('Login') }}
+            </span>
+        </button>
+      </a>
+    @else
+
+    <a class="flex-1 text-center" href="{{ route('profileView', Auth::user()->name) }}">
+      <button class="head-button">
+        <span class="icon-person text-white text-3xl"></span>
+        <span class="hidden md:block capitalize">
+          {{Auth::user()->name}}
+        </span>
+      </button>
+    </a>
+
+    @endguest
+
+  </div>
+</div>
