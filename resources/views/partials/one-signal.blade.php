1ca9c22e93729aad6f949ef4bc138a492fc7c23a
guillermo <gtamborero@iproject.cat>
2020-12-12 22:41:48 +0100

    Update one-signal.blade.php

--- resources/views/partials/one-signal.blade.php
+++ resources/views/partials/one-signal.blade.php
@@ -3,11 +3,21 @@
 <script>
   window.OneSignal = window.OneSignal || [];
   OneSignal.push(function() {
-
+    console.log("hi2");
     OneSignal.init({
-      appId: "9e810395-d013-4756-a74b-82f5b6f2848a",
+      appId: "8642fc9f-529a-43e7-9b0e-d82d490569e3",
     });
+
+    
+        //console.log(await OneSignal.getUserId());
+
     OneSignal.setExternalUserId({{Auth::user()->id}});
+          // user has subscribed
+          //OneSignal.registerForPushNotifications();
   });
+
+
+
+
 </script>
 @endauth
